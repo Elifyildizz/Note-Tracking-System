3f8942f5d8fb515e41a9671203fd6548e202ee7e
Elifyildizz <yildiz.elifffff@gmail.com>
2023-07-26 16:11:55 +0300

    Service Layer

--- Note-Tracking-System/src/main/java/com/notetracker/NoteTrackingSystem/NoteTrackingSystemApplication.java
+++ Note-Tracking-System/src/main/java/com/notetracker/NoteTrackingSystem/NoteTrackingSystemApplication.java
@@ -2,12 +2,19 @@ package com.notetracker.NoteTrackingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NoteTrackingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteTrackingSystemApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello world";
+	}
 }
