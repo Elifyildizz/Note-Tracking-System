bb2afe7a6d6548a31b543b8bcadaa9937ebb75bb
Elifyildizz <yildiz.elifffff@gmail.com>
2023-07-26 13:38:33 +0300

    Service Layer

--- Note-Tracking-System/src/main/java/com/notetracker/NoteTrackingSystem/controller/CourseController.java
+++ Note-Tracking-System/src/main/java/com/notetracker/NoteTrackingSystem/controller/CourseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
+@RestController
+@RequestMapping(path = "api/v1/courses")
 public class CourseController {
     private final CourseService courseService;
 
